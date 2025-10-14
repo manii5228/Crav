e0994c5c3fe843bd3b4357aad0ac8515c63d27f1
manii5228 <manimanjunath.v@gmail.com>
2025-10-14 23:07:56 +0530

    Delete backend directory

--- backend/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security
-from flask_restful import Api
-
-# Create the extension instances here
-db = SQLAlchemy()
-security = Security()
-api = Api()
