99351fec562d8f56dcf0d1cdde8c95c1d84085a5
manii5228 <manimanjunath.v@gmail.com>
2025-10-16 08:01:49 +0530

    Delete backend directory

--- backend/security.py
+++
@@ -1,6 +0,0 @@
-from flask_security import Security, SQLAlchemyUserDatastore
-from .models import db, User, Role
-
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security()
